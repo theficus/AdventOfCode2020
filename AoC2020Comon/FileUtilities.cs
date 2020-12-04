7e9fdc5498a26dbf6295f51a2442ca7728d8ec1f
Adam Meltzer <adam@theficus.com>
2020-12-04 14:00:50 -0800

    * .gitignore: initial skeleton
    * AoC2020.sln:
    * Program.cs:
    * Day1.csproj:
    * FileUtilities.cs:
    * AssemblyInfo.cs:
    * AoC2020Comon.csproj:
    * Day1Input.txt:
    * AssemblyInfo.cs:

---
+++ AoC2020Comon/FileUtilities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AoC2020Comon
+{
+    public static class FileUtilities
+    {
+        public static IEnumerable<string> GetFileContents(string fileName)
+        {
+            foreach (string s in File.ReadAllLines($"Content/{fileName}"))
+            {
+                yield return s;
+            }
+        }
+
+    }
+}
