6510067594eeafc2d89b15b68f2a7951025a1abd
Adam Meltzer <adam@theficus.com>
2020-12-17 22:05:05 -0800

    Day 8 skeleton

---
+++ Day8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Day8
+{
+    /// <summary>
+    /// Day 8
+    /// </summary>
+    /// <remarks>
+    /// https://adventofcode.com/2020/day/8
+    /// </remarks>
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
